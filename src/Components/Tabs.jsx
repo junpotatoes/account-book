25d3be18a768d531cd1104b8170bf42a54568fb0
Jangs <wkdwnsdlr37@gmail.com>
2022-11-29 21:28:32 +0900

    [Feat] Tabs컴포넌트 리팩토링

--- src/Components/Tabs.jsx
+++ src/Components/Tabs.jsx
@@ -3,72 +3,41 @@ import "../css/Tabs.css";
 // import Modal from "./Modal";
 import Calendar from './Calendar';
 import CalendarSub from "./CalendarSub";
+import Settings from "./Settings";
 
 function Tabs() {
-  const [toggleState, setToggleState] = useState(1);
+  const tapPage = ["캘린더", "월별 통계", "설정"];
+  const [currenTab, SetCurrenTab] = useState(0); 
+  console.log(currenTab)
+  
 
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
-
-  return (
-    <div className="big__Container">
-      <div className="left__Block"></div>
-      <div className="container">
-        <div className="bloc-tabs">
-          <button
-            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(1)}
-          >
-            캘린더
-          </button>
-          <button
-            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(2)}
-          >
-            월별통계
-          </button>
-          <button
-            className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(3)}
-          >
-            설정
-          </button>
-        </div>
-
-        <div className="content-tabs">
-          <div
-            className={
-              toggleState === 1 ? "content  active-content" : "content"
-            }
-          >
-            {/* <Modal /> */}
-            <div className="test">
-              <div>
-                <Calendar/>
-              </div>
-              <div>
-                <CalendarSub/>
-              </div>
-            </div>
+  return(
+    <div className="layOut">
+      <div>
+        <ul className="tap__List">
+          {tapPage.map((el, idx)=> 
+          <li
+           key={idx}
+           className="tap__List__Docs"
+           onClick={ () => SetCurrenTab(idx)}
+           >{el}</li>
+          )}
+        </ul>
+        <div>
+          <div hidden={currenTab !== 0}>
+            {/* <p>캘린더 관련 컴포넌트들</p> */}
+            <Calendar/>
           </div>
-
-          <div
-            className={
-              toggleState === 2 ? "content  active-content" : "content"
-            }
-          ></div>
-
-          <div
-            className={
-              toggleState === 3 ? "content  active-content" : "content"
-            }
-          ></div>
+          <div hidden={currenTab !== 1}>
+            {/* <p>월별 통계 관련 컴포넌트들</p> */}
+          </div>
+          <div hidden={currenTab !== 2}>
+            {/* <p>설정 관련 컴포넌트들</p> */}
+          </div>
         </div>
       </div>
-      <div className="right__Block"></div>
     </div>
-  );
+  )
 }
-
+  
 export default Tabs;

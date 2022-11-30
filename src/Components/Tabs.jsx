7c9ca10246c53b66c329efb9a46773f3bc3cb354
Jangs <wkdwnsdlr37@gmail.com>
2022-11-30 17:56:23 +0900

    [Feat]전체적인 레이아웃

--- src/Components/Tabs.jsx
+++ src/Components/Tabs.jsx
@@ -1,43 +1,43 @@
 import { useState } from "react";
 import "../css/Tabs.css";
 // import Modal from "./Modal";
-import Calendar from './Calendar';
+import Calendar from "./Calendar";
 import CalendarSub from "./CalendarSub";
 import Settings from "./Settings";
 
 function Tabs() {
   const tapPage = ["캘린더", "월별 통계", "설정"];
-  const [currenTab, SetCurrenTab] = useState(0); 
-  console.log(currenTab)
-  
+  const [currenTab, SetCurrenTab] = useState(0);
 
-  return(
-    <div className="layOut">
-      <div>
-        <ul className="tap__List">
-          {tapPage.map((el, idx)=> 
+  return (
+    <div className="layout">
+      <ul className="tap__title">
+        {tapPage.map((el, idx) => (
           <li
-           key={idx}
-           className="tap__List__Docs"
-           onClick={ () => SetCurrenTab(idx)}
-           >{el}</li>
-          )}
-        </ul>
-        <div>
-          <div hidden={currenTab !== 0}>
-            {/* <p>캘린더 관련 컴포넌트들</p> */}
-            <Calendar/>
+            key={idx}
+            className="tap__list__docs"
+            onClick={() => SetCurrenTab(idx)}
+          >
+            {el}
+          </li>
+        ))}
+      </ul>
+      <main className="tap_content">
+          <div className={`${currenTab !== 0 ? "Dn" : "flex__row"}`}>
+              <div className="flex__3"><Calendar/></div>
+              <div className="flex__1"><CalendarSub/></div>
           </div>
-          <div hidden={currenTab !== 1}>
-            {/* <p>월별 통계 관련 컴포넌트들</p> */}
+          <div className={`${currenTab !== 1 ? "Dn" : "flex__column"}`}>
+              <div className="flex__1">{/*컴포넌트1*/}</div>
+              <div className="flex__1">{/*컴포넌트2*/}</div>
           </div>
-          <div hidden={currenTab !== 2}>
-            {/* <p>설정 관련 컴포넌트들</p> */}
+          <div className={`${currenTab !== 2 ? "Dn" : "flex__column"}`}>
+              <div className="flex__1">{/*컴포넌트1*/}</div>
+              <div className="flex__1">{/*컴포넌트2*/}</div>
           </div>
-        </div>
-      </div>
+      </main>
     </div>
-  )
+  );
 }
-  
+
 export default Tabs;

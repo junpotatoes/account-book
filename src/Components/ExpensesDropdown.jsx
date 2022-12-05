931b5275ed6e47d2b2ace572e5bc698685888977
junpotatoes <mukdol1127@naver.com>
2022-12-05 10:23:17 +0900

    [docs] Expenses value change

--- src/Components/ExpensesDropdown.jsx
+++ src/Components/ExpensesDropdown.jsx
@@ -7,12 +7,10 @@ function SelectBasicExample() {
       <option value="1">식비</option>
       <option value="2">교통비</option>
       <option value="3">문화생활</option>
-      <option value="4">마트/편의점</option>
-      <option value="5">패션/미용</option>
-      <option value="6">생활용품</option>
-      <option value="7">건강</option>
-      <option value="8">경조사비</option>
-      <option value="8">기타</option>
+      <option value="4">패션/미용</option>
+      <option value="5">생활용품</option>
+      <option value="6">주거/통신</option>
+      <option value="7">기타</option>
     </Form.Select>
   );
 }
